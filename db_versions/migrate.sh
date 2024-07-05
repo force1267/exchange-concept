9acce68655833123281d483eaab3e7b80b64e1d1
MohammadJavad Asadi <mohammadjavad.asadi@tapsi.cab>
2024-07-05 03:31:19 +0330

    Add postgres migration directory

---
+++ db_versions/migrate.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+
+HELP='''
+    run this script to apply all sql files in this directory to a postgres database
+    ./migrate.sh localhost 5432
+    or without any arguments to default to postgres:5432
+    ./migrate.sh
+
+    --wait 5          waits 5 seconds before starting
+
+    local dev version:
+    PGPASSWORD=12345678 ./migrate.sh localhost 5432
+
+    with docker compose:
+    ./migrate.sh --wait
+    
+    production environment:
+    ./migrate.sh $HOST $PORT
+'''
+
+DEFAULT_WAIT=5
+WAIT=0
+POSITIONAL_ARGS=()
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    --wait)
+        if [[ "$2" == "" ]]; then
+            WAIT=$DEFAULT_WAIT
+        else
+            WAIT=$2
+            shift
+        fi
+        shift
+        ;;
+    -h|--help)
+      echo $HELP
+      exit 0
+      ;;
+    -*|--*)
+      echo "Unknown option $1"
+      exit 1
+      ;;
+    *)
+      POSITIONAL_ARGS+=("$1")
+      shift
+      ;;
+  esac
+done
+
+set -- "${POSITIONAL_ARGS[@]}"
+
+
+HOST=${1:-postgres}
+PORT=${2:-5432}
+
+if [[ "$WAIT" != "0" ]]; then
+    echo waiting 5 seconds for database to come up
+fi
+sleep $WAIT
+
+for sql in $(ls | grep .sql); do
+    echo runing $sql:
+    psql --host=$HOST --port=$PORT --user=postgres --file=$(realpath $sql)
+done
